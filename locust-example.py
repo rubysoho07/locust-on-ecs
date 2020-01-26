913c008e58e1c6a36d4c5c66229fb6ac472a223a
Yungon Park <hahafree12@gmail.com>
2020-01-26 13:55:35 +0900

    예제 내용 단순화

--- locust-example.py
+++ locust-example.py
@@ -3,13 +3,9 @@ from locust import HttpLocust, TaskSet, task, between
 
 class MyTaskSet(TaskSet):
 
-    @task(2)
+    @task
     def get_index_page(self):
         self.client.get('/')
-
-    @task(1)
-    def get_detail_page(self):
-        self.client.get('/musicmanager/album/1/')
 
 
 class MyLocust(HttpLocust):
